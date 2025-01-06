9d2582d7f1fa724bf66be0516247e29063b27ec4
sahasrareddyreddy <reddyarikatlasahasra@gmail.com>
2025-01-06 12:20:26 +0530

    Add files via upload

---
+++ Demo007.java
@@ -0,0 +1,16 @@
+package Core_Java;
+import java.util.Scanner;
+public class Demo007 {
+    public static void main(String[] args){
+       String name;
+       int age;
+       String Phno;
+       System.out.println(x:"plz enter ur name, age, and phone no");
+       Scanner sc = new Scanner(System.in);
+       name = sc.nextLine();
+       age = sc.nextInt();
+       phno = sc.nextLine();
+       System.out.println("hello "+name +"ur age is "+ age+ "ur phone no is "+ phno);
+
+    }
+}

c459d11c48062603aef2f3c9d304c8e885898273
sahasrareddyreddy <reddyarikatlasahasra@gmail.com>
2025-01-27 12:18:28 +0530

    Happy pongal

---
+++ test002.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class test002 {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.print("Enter your reply (yes/no): ");
+                
+                if (scanner.next().equalsIgnoreCase("yes")) {
+                    System.out.println("Happy Pongal!");
+                    break;
+                } else if (scanner.next().equalsIgnoreCase("no")) {
+                    System.out.println("Error");
+                    break;
+                
+            }
+        }
+    }
+}
+}

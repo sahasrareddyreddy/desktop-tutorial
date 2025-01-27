ba5e88f4c79660162fb83b942e78a56f863f6519
sahasrareddyreddy <reddyarikatlasahasra@gmail.com>
2025-01-27 12:13:30 +0530

    next multiple

---
+++ ps002.java
@@ -0,0 +1,19 @@
+ package Core_Java;
+class NextMultipleOf100 {
+    
+    public static int getNextMultipleOf100(int num) {
+         if (num <= 0)
+             return -1;
+         return ((num / 100) + 1) * 100;
+    }
+}
+
+public class ps002 {
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        NextMultipleOf100 obj = new NextMultipleOf100();
+        int num = 223;
+        System.out.println(NextMultipleOf100.getNextMultipleOf100(num));
+    }
+    
+}

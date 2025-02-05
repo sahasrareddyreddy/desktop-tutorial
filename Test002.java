79c92125ce541c362620562bd9a24223e96c8df6
sahasrareddyreddy <reddyarikatlasahasra@gmail.com>
2025-02-05 12:33:09 +0530

    Happy Pongal

--- Test002.java
+++ Test002.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class test002 {
+public class Test002 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             while (true) {

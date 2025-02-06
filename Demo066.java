d387cc4fe4ad2377c39325d496314e8656b5a27b
sahasrareddyreddy <reddyarikatlasahasra@gmail.com>
2025-02-06 12:59:45 +0530

    priniting the indices of an array

---
+++ Demo066.java
@@ -0,0 +1,9 @@
+public class Demo066 {
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 30, 40, 50 };
+        System.out.print("Indices are: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(i + " ");
+        }
+    }
+};

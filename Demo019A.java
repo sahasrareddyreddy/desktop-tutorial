45459841ae9072776f75aaf27f25f038fd36df06
sahasrareddyreddy <reddyarikatlasahasra@gmail.com>
2025-02-10 12:42:12 +0530

    rounded figure

--- Demo019A.java
+++ Demo019A.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Demo019 {
+public class Demo019A {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");

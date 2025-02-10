154c06a44e746d892d3ee63401f5b08820c23a59
sahasrareddyreddy <reddyarikatlasahasra@gmail.com>
2025-02-10 10:53:16 +0530

    to find maximum number of an array

---
+++ Demo075.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Demo075 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Array size must be greater than 0.");
+            return;
+        }
+
+        int[] numbers = new int[n];
+
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        int max = numbers[0];
+        for (int i = 1; i < n; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("The maximum number in the array is: " + max);
+
+        scanner.close();
+    }
+}

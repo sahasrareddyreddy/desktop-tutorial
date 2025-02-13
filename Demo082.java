ec38550efcc17d5d32a79c72ff9135a1ac4252a5
sahasrareddyreddy <reddyarikatlasahasra@gmail.com>
2025-02-13 12:52:22 +0530

    to merge to arrays and copy into another array

---
+++ Demo082.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Demo082 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of elements in first array: ");
+        int n1 = scanner.nextInt();
+        int[] arr1 = new int[n1];
+        System.out.println("Enter elements of first array:");
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = scanner.nextInt();
+        }
+        
+        System.out.print("Enter the number of elements in second array: ");
+        int n2 = scanner.nextInt();
+        int[] arr2 = new int[n2];
+        System.out.println("Enter elements of second array:");
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = scanner.nextInt();
+        }
+        
+        int[] mergedArray = new int[n1 + n2];
+        for (int i = 0; i < n1; i++) {
+            mergedArray[i] = arr1[i];
+        }
+        for (int i = 0; i < n2; i++) {
+            mergedArray[n1 + i] = arr2[i];
+        }
+        
+        System.out.println("Merged array:");
+        for (int num : mergedArray) {
+            System.out.print(num + " ");
+        }
+    }
+}

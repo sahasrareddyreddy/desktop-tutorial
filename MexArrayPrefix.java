3e20e94195404442c21bc36153dc72e68138c12d
sahasrareddyreddy <reddyarikatlasahasra@gmail.com>
2025-02-13 12:22:32 +0530

    find the mex of an array

---
+++ MexArrayPrefix.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class MexArrayPrefix {
+    public static void findMexPrefix(int[] arr) {
+        int n = arr.length;
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> seen = new HashSet<>();
+        int mex = 0;
+
+        for (int num : arr) {
+            seen.add(num);
+            while (seen.contains(mex)) {
+                mex++;
+            }
+            result.add(mex);
+        }
+
+        for (int value : result) {
+            System.out.print(value + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        findMexPrefix(arr);
+    }
+}
